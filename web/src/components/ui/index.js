482d8e1ea70ddbfc3b77f1b8d0b513abf860009d
ahmertsengol <21sandn21l@gmail.com>
2025-05-13 02:02:12 +0300

    Fix whitespace in UI components index

--- web/src/components/ui/index.js
+++ web/src/components/ui/index.js
@@ -2,5 +2,5 @@
 export { default as AnimatedElement } from './AnimatedElement';
 export { default as GlassmorphicCard } from './GlassmorphicCard';
 export { default as AudioDecorations } from './AudioDecorations';
-export { AudioEqualizer, AudioWave, AudioVisualizer, AudioBarGraph } from './AudioDecorations';
+export { AudioEqualizer, AudioWave, AudioVisualizer, AudioBarGraph } from './AudioDecorations'; 
 export { default as MusicChatbot } from './MusicChatbot'; 
