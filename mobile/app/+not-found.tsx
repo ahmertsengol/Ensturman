dadcf05bae1d314176644936fbd5daa1525d5e9e
ahmertsengol <21sandn21l@gmail.com>
2025-05-19 19:37:12 +0300

    404 sayfası güncellendi

--- mobile/app/+not-found.tsx
+++ mobile/app/+not-found.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Link, Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
@@ -11,7 +12,7 @@ export default function NotFoundScreen() {
       <ThemedView style={styles.container}>
         <ThemedText type="title">This screen does not exist.</ThemedText>
         <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+          <ThemedText type="default">Go to home screen!</ThemedText>
         </Link>
       </ThemedView>
     </>
