15b7c3b6568eaaafc40ab7f93e389cf3123c14e4
ahmertsengol <21sandn21l@gmail.com>
2025-05-19 19:37:07 +0300

    Ana sayfa güncellendi

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -23,9 +23,9 @@ export default function HomeScreen() {
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+          Edit <ThemedText type="default" style={{fontWeight: '600'}}>app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
-          <ThemedText type="defaultSemiBold">
+          <ThemedText type="default" style={{fontWeight: '600'}}>
             {Platform.select({
               ios: 'cmd + d',
               android: 'cmd + m',
@@ -45,10 +45,10 @@ export default function HomeScreen() {
         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
         <ThemedText>
           {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+          <ThemedText type="default" style={{fontWeight: '600'}}>npm run reset-project</ThemedText> to get a fresh{' '}
+          <ThemedText type="default" style={{fontWeight: '600'}}>app</ThemedText> directory. This will move the current{' '}
+          <ThemedText type="default" style={{fontWeight: '600'}}>app</ThemedText> to{' '}
+          <ThemedText type="default" style={{fontWeight: '600'}}>app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
